69cf4ea5b17c497c5cea0cd02a1c4db838d7887a
Mijk <dipalma10@hotmail.com>
2023-09-28 10:35:03 +0200

    Add files via upload

---
+++ Create-Task-Schedule.ps1
@@ -0,0 +1,6 @@
+﻿$tempdir = "C:\Windows\temp\PowerShell-PC-Inventory"
+New-Item $tempdir -ItemType Directory -Force
+New-Item "$tempdir\InventoryOutput" -ItemType Directory -Force
+Copy-Item ".\PowerShell-PC-Inventory.ps1" -Destination $tempdir -Force
+Copy-Item ".\RunInventoryeveryday.xml" -Destination $tempdir -Force
+Register-ScheduledTask -xml (Get-Content '.\RunInventoryeveryday.xml' | Out-String) -TaskName "Run Inventory every day" -Force
